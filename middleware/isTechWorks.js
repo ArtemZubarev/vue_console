132153fd33361536d1b835d957d9dff262859895
Artem Zubarev <xagrssr@gmail.com>
2022-04-01 12:18:50 +0300

    tech works test

--- middleware/isTechWorks.js
+++ middleware/isTechWorks.js
@@ -2,14 +2,11 @@ export const techWorks = true
 
 export default function ({ redirect, app }) {
   const isTester = app.$cookies.get('forTests') === 'tester'
+  const isTechWorksPage = app.router.currentRoute.path.includes('/tech-works')
 
   if (!isTester) {
-    if (techWorks) {
-      console.log(23)
+    if (techWorks && !isTechWorksPage) {
       return redirect(app.localePath('/tech-works'))
     }
-    // if (!techWorks && isTechWorksPage) {
-    //   return redirect(app.localePath('/'))
-    // }
   }
 }
