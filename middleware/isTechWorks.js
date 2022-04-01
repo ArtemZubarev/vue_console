324ac3b29a08aba40d3fb9846eb9cb5893d899ba
Artem Zubarev <xagrssr@gmail.com>
2022-04-01 13:00:29 +0300

    middleware

--- middleware/isTechWorks.js
+++ middleware/isTechWorks.js
@@ -1,8 +1,10 @@
 export const techWorks = true
 
 export default function ({ redirect, app }) {
-  // const isTester = app.$cookies.get('forTests') === 'tester'
-  // const isTechWorksPage = app.router.currentRoute.path.includes('/tech-works')
+  const isTester = app.$cookies.get('forTests') === 'tester'
+  const isTechWorksPage = app.router.currentRoute.path.includes('/tech-works')
+  console.log(isTester)
+  console.log(isTechWorksPage)
 
   // if (!isTester) {
   //   if (techWorks && !isTechWorksPage) {
